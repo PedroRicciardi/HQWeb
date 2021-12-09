d311bbf28bbaa398adba1aa38bfeedf8d95392b4
PedroRicciardi <pedrohenriquericciardi@gmail.com>
2021-12-09 17:07:08 -0300

    Criando uma conexão com o banco

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,10 @@ namespace FrontHQWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkNpgsql()
+            .AddDbContext<>(options => 
+            options.UseNpgsql(Configuration.GetConnectionString("ConnectionString")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "swagger", Version = "v1" });
